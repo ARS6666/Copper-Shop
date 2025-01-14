1befb6ab31af06b9c4ea2fda21a5a761cc29865b
ARS6666 <ARSprogramming123@gmail.com>
2025-01-13 21:38:23 -0800

    loading finished

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -4,9 +4,9 @@ import Img from "../../assets/media/sini.png";
 
 const Loading = () => {
         return (
-                <div className="loader fontr rowm-0">
+                <div className="loader fontr">
                         <img src={Img} alt="Loading" className="loader-image col-md-12" />
-                        <span>... صبور باشید</span>
+                        <span>صبور باشید ...</span>
                 </div>
         );
 };
