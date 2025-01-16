35deab1bdb73ef48a0e857907f5002ff3d9cbadc
ARS6666 <ARSprogramming123@gmail.com>
2025-01-16 10:16:45 -0800

    blogs finished

--- src/components/blog/blogpage.js
+++ src/components/blog/blogpage.js
@@ -12,13 +12,15 @@ const Blog = ({ blogId }) => {
         <div className='col-md-12 fontr' dir="rtl">
             <div className='col-md-12 pt-3'>
                 <div className='col-md-12 text-white d-flex justify-content-center align-items-center' style={{ height: "80px", backgroundColor: "#DB5C28"}}>
-                    <h2>{blog.title}</h2>
+                    <h2 className='align-self-center'>{blog.title}</h2>
                 </div>
                 <div className='container pt-3'>
                     <p className='h5'style={{lineHeight:"2rem",wordSpacing : "0.7rem" }}>{blog.content}</p>
                 </div>
+                <div className='container pt-3 d-flex justify-content-center pb-4'>
+                    <img className="blogImg" src={Img}/>
+                </div>
             </div>
-            <img/>
         </div>
     </>);
 };
