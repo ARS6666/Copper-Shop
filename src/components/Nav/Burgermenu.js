75b5e049a6ecd9b04fef84d6eeb566baa1a40871
ARS6666 <ARSprogramming123@gmail.com>
2025-02-12 10:15:42 -0800

    imporving Burgermenu.js

--- src/components/Nav/Burgermenu.js
+++ src/components/Nav/Burgermenu.js
@@ -11,32 +11,42 @@ const BurgerMenu = (theme) => {
         document.body.style.overflow = isOpen ? 'auto' : 'hidden';
     };
 
+    const closeMenu = () => {
+        setIsOpen(false);
+        document.body.style.overflow = 'auto';
+    };
+
     return (
         <div dir="rtl" className="fontr bg-orange">
             <div className={`overlayy ${isOpen ? 'show' : ''}`}></div>
             <button className="btn btn-transparent" onClick={toggleMenu} aria-label="Toggle Menu">
                 <i className="text-white fa-solid fa-bars" style={{ fontSize: "1.4rem" }}></i>
             </button>
-            <div className={theme.theme.theme === "dark" ? `menu ${isOpen ? 'open' : ''}` : `text-dark menu ${isOpen ? 'open' : ''}`} style={{ backgroundColor: theme.theme.theme === "dark" ? "#121212" : "white" }}>
+            <div className={`bg-orange text-white menu ${isOpen ? 'open' : ''}`}>
                 <div className="col-12 d-flex justify-content-start">
-                    <button className="btn btn-transparent" onClick={toggleMenu} aria-label="Close Menu">
-                        <i className={theme.theme.theme === "dark" ? "text-white fa-solid fa-xmark" : "text-dark fa-solid fa-xmark"} style={{ fontSize: "1.1rem" }}></i>
+                    <button className="btn btn-transparent" onClick={closeMenu} aria-label="Close Menu">
+                        <i className="text-white fa-solid fa-xmark" style={{ fontSize: "1.1rem" }}></i>
                     </button>
                 </div>
                 <ul>
                     <li className="col-12 d-flex justify-content-start">
-                        <Link className={theme.theme.theme === "dark" ? "hrefw" : "hrefb"} to="/">
+                        <Link className="hrefw" to="/" onClick={closeMenu}>
                             <span className="col-3 h5 ah">خانه</span>
                         </Link>
                     </li>
                     <li className="col-12 d-flex justify-content-start">
-                        <Link className={theme.theme.theme === "dark" ? "hrefw" : "hrefb"} to="/products">
+                        <Link className="hrefw" to="/products" onClick={closeMenu}>
                             <span className="col-3 h5 ah">محصولات</span>
                         </Link>
                     </li>
                     <li className="col-12 d-flex justify-content-start">
-                        <Link className={theme.theme.theme === "dark" ? "hrefw" : "hrefb"} to="/about">
-                            <span className="col-3 h5 ah">درباره ما</span>
+                        <Link className="hrefw" to="/blogs" onClick={closeMenu}>
+                            <span className="col-3 h5 ah">وبلاگ</span>
+                        </Link>
+                    </li>
+                    <li className="col-12 d-flex justify-content-start">
+                        <Link className="hrefw" to="/about" onClick={closeMenu}>
+                            <span className="col-3 h5 ah">خدمات مشتریان</span>
                         </Link>
                     </li>
                     <li className="col-12 row m-0">
@@ -45,14 +55,14 @@ const BurgerMenu = (theme) => {
                                 className="form-control fontr col-12"
                                 placeholder="جست وجو ..."
                                 onChange={(e) => setSearch(e.target.value)}
-                                style={{ backgroundColor: "#D9D9D9" }}
+                                style={{ backgroundColor: "white" }}
                                 aria-label="Search"
                             />
                         </div>
                         <div className="col-1 align-self-center">
-                            <Link to={`/products?search=${search}`} className={theme.theme.theme === "dark" ? "hrefw" : "hrefb"}>
+                            <Link to={`/products?search=${search}`} className="hrefw" onClick={closeMenu}>
                                 <button
-                                    className={theme.theme.theme === "dark" ? "rounded-circle btn bg-transparent align-self-center col-6 text-white" : "rounded-circle btn bg-transparent align-self-center col-6"}
+                                    className="rounded-circle btn bg-transparent align-self-center col-6 text-white"
                                     aria-label="Search Button"
                                     style={{ backgroundColor: "#E8E7E7" }}
                                 >
